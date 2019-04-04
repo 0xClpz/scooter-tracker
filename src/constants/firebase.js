0ad7aed72feffb8eee14359c621a5e65e621213d
Antonio <antonio.root@gmail.com>
2019-04-04 22:26:34 +0200

    Initial commit

---
+++ src/constants/firebase.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDaqIOj0_Sh5_-UfBLMAOLxmsmPKsrfkbk",
+  authDomain: "scooter-tracker-e4a80.firebaseapp.com",
+  databaseURL: "https://scooter-tracker-e4a80.firebaseio.com",
+  projectId: "scooter-tracker-e4a80",
+  storageBucket: "scooter-tracker-e4a80.appspot.com",
+  messagingSenderId: "19849491991"
+};
