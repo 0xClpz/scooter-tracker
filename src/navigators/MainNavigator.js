5d9192c92ec4c761a7cbf82f841bdd8f0933ff48
Antonio <antonio.root@gmail.com>
2019-04-06 15:36:16 +0200

    Adds icons for each tab

--- src/navigators/MainNavigator.js
+++ src/navigators/MainNavigator.js
@@ -3,10 +3,33 @@ import {Main} from '../screens/main/Main'
 import {Settings} from '../screens/settings/Settings'
 import {Stats} from '../screens/stats/Stats'
 import {History} from '../screens/rides/History'
+import React from 'react'
+import {Ionicons} from '@expo/vector-icons'
 
-export const MainNavigator = createBottomTabNavigator({
-  Main,
-  Stats,
-  History,
-  Settings,
-})
+const IconMap = {
+  Main: 'ios-add',
+  Stats: 'ios-stats',
+  History: 'ios-journal',
+  Settings: 'ios-cog',
+}
+
+export const MainNavigator = createBottomTabNavigator(
+  {
+    Main,
+    Stats,
+    History,
+    Settings,
+  },
+  {
+    defaultNavigationOptions: ({navigation}) => ({
+      // eslint-disable-next-line
+      tabBarIcon: ({focused, horizontal, tintColor}) => {
+        const {routeName} = navigation.state
+        // You can return any component that you like here!
+        return (
+          <Ionicons name={IconMap[routeName]} size={25} color={tintColor} />
+        )
+      },
+    }),
+  }
+)
